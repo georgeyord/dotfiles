db7c69bdc0a9fc3c3b35fbaa8caa6171f304eaa7
George Yord <george.yord@gmail.com>
2018-11-15 16:32:56 +0200

    Enhance git status in touchbar

--- .zsh-custom/iterm.zsh
+++ .zsh-custom/iterm.zsh
@@ -40,8 +40,40 @@ NAME="byobu"
 ~/.iterm2/it2setkeylabel set F17 "17"
 ~/.iterm2/it2setkeylabel push "${NAME}"
 
+git_status_for_touchbar() {
+  source /opt/codebase/others/zsh-git-prompt/zshrc.sh
+  precmd_update_git_vars
+  if [ -n "$__CURRENT_GIT_STATUS" ]; then
+    DELIMETER=" "
+    STATUS="$GIT_BRANCH"
+    
+    if [ "$GIT_BEHIND" -ne "0" ]; then
+      STATUS="$STATUS$DELIMETER↓$GIT_BEHIND"
+    fi
+    if [ "$GIT_AHEAD" -ne "0" ]; then
+      STATUS="$STATUS$DELIMETER↑$GIT_AHEAD"
+    fi
+    if [ "$GIT_STAGED" -ne "0" ]; then
+      STATUS="$STATUS$DELIMETER●$GIT_STAGED"
+    fi
+    if [ "$GIT_CONFLICTS" -ne "0" ]; then
+      STATUS="$STATUS$DELIMETER✖$GIT_CONFLICTS"
+    fi
+    if [ "$GIT_CHANGED" -ne "0" ]; then
+      STATUS="$STATUS$DELIMETER✚$GIT_CHANGED"
+    fi
+    if [ "$GIT_UNTRACKED" -ne "0" ]; then
+      STATUS="$STATUS$DELIMETER…$GIT_UNTRACKED"
+    fi
+    if [ "$GIT_CHANGED" -eq "0" ] && [ "$GIT_CONFLICTS" -eq "0" ] && [ "$GIT_STAGED" -eq "0" ] && [ "$GIT_UNTRACKED" -eq "0" ]; then
+      STATUS="$STATUS$DELIMETER✔"
+    fi
+    echo "$STATUS"
+  fi
+}
+
 function setGitStatusInTouchbar() {
-    ~/.iterm2/it2setkeylabel set status "$(test -d .git && (git rev-parse --abbrev-ref HEAD) || (echo -n "Not a repo"))"
+    ~/.iterm2/it2setkeylabel set status "$(git_status_for_touchbar)"
 }
 
 function setFnLabels() {

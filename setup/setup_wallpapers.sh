a35e798f3444899012e01c6feb976f3106771e5c
George Yord <george.yord@gmail.com>
2019-01-23 11:56:37 +0200

    Wallpaper optimization

--- setup/setup_wallpapers.sh
+++ setup/setup_wallpapers.sh
@@ -13,22 +13,20 @@ function preview() {
   fi
 }
 
-if hash wallpaper 2> /dev/null ; then
-  if [ -f "${WALLPAPER}" ]; then
-    SCREEN=0
-    preview "${WALLPAPER}" "${SCREEN}"
-    wallpaper set --screen "${SCREEN}" "${WALLPAPER}"
-  fi
-  if [ -f "${WALLPAPER_1}" ]; then
-    SCREEN=1
-    preview "${WALLPAPER_1}" "${SCREEN}"
-    wallpaper set --screen "${SCREEN}" "${WALLPAPER_1}"
-  fi
-  if [ -f "${WALLPAPER_2}" ]; then
-    SCREEN=2
-    preview "${WALLPAPER_2}" "${SCREEN}"
-    wallpaper set --screen "${SCREEN}" "${WALLPAPER_2}"
-  fi
-else
-  echo "wallpaper app is not installed, please run 'brew install wallpaper' first!"
+type wallpaper 2> /dev/null || brew install wallpaper
+
+if [ -f "${WALLPAPER}" ]; then
+  SCREEN=0
+  preview "${WALLPAPER}" "${SCREEN}"
+  wallpaper set --screen "${SCREEN}" "${WALLPAPER}"
+fi
+if [ -f "${WALLPAPER_1}" ]; then
+  SCREEN=1
+  preview "${WALLPAPER_1}" "${SCREEN}"
+  wallpaper set --screen "${SCREEN}" "${WALLPAPER_1}"
+fi
+if [ -f "${WALLPAPER_2}" ]; then
+  SCREEN=2
+  preview "${WALLPAPER_2}" "${SCREEN}"
+  wallpaper set --screen "${SCREEN}" "${WALLPAPER_2}"
 fi

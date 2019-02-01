c41c6e166e116c80cf0549e3221302bfef646f2f
George Yord <george.yord@gmail.com>
2019-02-01 12:53:42 +0200

    Fix fzf

--- .zsh-custom/tab-completion.zsh
+++ .zsh-custom/tab-completion.zsh
@@ -11,4 +11,4 @@
 # # Add `killall` tab completion for common apps
 # complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall;
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zs
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh

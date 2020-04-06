3b8081600de4aa4162fedebb6bb9c05b5abe1ab7
George Yord <george.yord@gmail.com>
2020-04-06 16:47:20 +0300

    De-actrivate Bitrix installation

--- setup/apps/07-communication.sh
+++ setup/apps/07-communication.sh
@@ -8,10 +8,10 @@ brew cask install viber
 brew cask install skype
 brew cask install zoomus
 
-if [ ! -d /Applications/Bitrix24.app ]; then
-  curl http://dl.bitrix24.com/b24/bitrix24_desktop.dmg -o /tmp/bitrix24_desktop.dmg
-  open /tmp/bitrix24_desktop.dmg
-  instruct "Finalize the bitrix installation manually..."
-fi
+# if [ ! -d /Applications/Bitrix24.app ]; then
+#   curl http://dl.bitrix24.com/b24/bitrix24_desktop.dmg -o /tmp/bitrix24_desktop.dmg
+#   open /tmp/bitrix24_desktop.dmg
+#   instruct "Finalize the bitrix installation manually..."
+# fi
 
 set +x

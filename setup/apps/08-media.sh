d1845746f263d6042f05f11892fd2e8534114dfd
George Yord <george.yord@gmail.com>
2021-03-06 09:55:47 +0200

    Introduce handbrake/makemkv

--- setup/apps/08-media.sh
+++ setup/apps/08-media.sh
@@ -3,13 +3,15 @@ set -e
 
 set -x
 
-brew cask install spotify
-brew cask install gimp
+is-installed-application "Spotify" || brew cask install spotify
+is-installed-application "Gimp" || brew cask install gimp
+hash HandBrakeCLI 2> /dev/null || brew cask install handbrake
+hash makemkvcon 2> /dev/null || brew cask install makemkv
 
 hash vlc 2> /dev/null || brew cask install vlc
 
 # Torrent client
-brew cask install folx
+is-installed-application "Folx" || brew cask install folx
 
 hash youtube-dl 2> /dev/null || brew install youtube-dl
 
